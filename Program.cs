23df8b919756f16ac03310715feb539bc07de5c2
Safin-Sarker <safinsarker1122@gmail.com>
2024-03-26 17:19:22 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ProblemSolvingForInterview
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string");
+            string input= Console.ReadLine();
+
+            if(IsPalindrome(input)) 
+            {
+                Console.WriteLine("String is palindromic");
+
+            }
+            else
+            {
+                Console.WriteLine("String is not palindromic");
+            }
+        }
+
+        public static bool IsPalindrome (string c)
+        {
+
+            string normalizestring = "";
+
+            foreach(char s in c)
+            {
+                if(char.IsLetterOrDigit(s)) 
+                {
+                    normalizestring += char.ToLower(s);
+                }
+            }
+
+            for (int i = 0; i < normalizestring.Length / 2; i++)
+            {
+                if (normalizestring[i] != normalizestring[normalizestring.Length-i - 1])
+                {
+                    return false;
+                }
+
+            }
+            return true;
+
+        }
+    }
+}
